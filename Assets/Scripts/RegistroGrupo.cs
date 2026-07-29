3fba3262767ab54f26a0ab7d59bfb78b5a1d8e62
LauTDel <tatianagd200113@gmail.com>
2026-07-28 23:12:05 -0500

    Inicio del script RegistroGrupo.cs por Laura Tatiana Delgado Garcia

---
+++ Assets/Scripts/RegistroGrupo.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class NewEmptyCSharpScript
+{
+    //	Nombre	completo:[Laura Tatiana Delgado Garcia]	
+    //	Fecha:	[28/07/2026]	
+    //	Hora:	[23:11]	
+
+    //No hay mas integrantes
+    
+}
